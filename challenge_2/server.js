142ae2169ba0aba1a035fa38b4a3ea7673814a97
Hack Reactor Student <student@hackreactor.com>
2018-10-16 12:31:34 -0700

    working on req.body

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,23 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+app.use(morgan('dev'));
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('./client'));
+
+app.post('/csv', (req, res) => {
+  console.log(typeof req.body)
+  console.log(req.body);
+  res.end()
+})
+
+app.listen(3000, () => {
+  console.log('listening on ', 3000)
+});
+
 
 
