ee8e946f709813149e7fd43facb52a8edae29fb5
Hack Reactor Student <student@hackreactor.com>
2018-10-17 21:03:55 -0700

    mongoDB should be working

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -4,7 +4,15 @@ var morgan = require('morgan');
 
 // connect to mongodb
 const MongoClient = require('mongodb').MongoClient;
-
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'account_info';
+ 
+// Use connect method to connect to the server
 
 var app = express();
 
@@ -12,6 +20,19 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.post('/purchase', (req, res) => {
+
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+   
+    const db = client.db(dbName);
+
+    db.collection(dbName).insertOne(req.body)
+    .then((response) => {assert.equal(1, response.insertedCount)});
+   
+    client.close();
+  });
+
   res.status(201).send();
 })
 
